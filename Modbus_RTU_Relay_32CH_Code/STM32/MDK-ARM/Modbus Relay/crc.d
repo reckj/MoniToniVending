8b8140ae5fb94e2615b48e9d3620fbcaa7034c4b
reckj <reckj@monitoni.com>
2025-12-08 15:01:35 +0100

    Remove Waveshare demo code from repo

--- Modbus_RTU_Relay_32CH_Code/STM32/MDK-ARM/Modbus Relay/crc.d
+++
@@ -1,3 +0,0 @@
-modbus relay\crc.o: ..\Src\crc.c
-modbus relay\crc.o: ../Inc/crc.h
-modbus relay\crc.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
--- Modbus_RTU_Relay_32CH_Code/STM32/MDK-ARM/Modbus Relay/modbus_crc.d
+++
@@ -1,3 +0,0 @@
-modbus relay\modbus_crc.o: ..\Src\modbus_crc.c
-modbus relay\modbus_crc.o: ../Inc/modbus_crc.h
-modbus relay\modbus_crc.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
